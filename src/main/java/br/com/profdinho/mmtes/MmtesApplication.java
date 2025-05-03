2f16da0e42545a0c1eafd001ab7258af5b9212d6
Prof. Dinho Zanardi <prof.dinho.ti@gmail.com>
2025-05-03 16:01:12 -0300

    Initial commit

---
+++ src/main/java/br/com/profdinho/mmtes/MmtesApplication.java
@@ -0,0 +1,13 @@
+package br.com.profdinho.mmtes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmtesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmtesApplication.class, args);
+	}
+
+}
